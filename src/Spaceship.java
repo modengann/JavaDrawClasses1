6410bb665d120f5cb2bde2c8f16678518c32dcdc
modengann <99417989+modengann@users.noreply.github.com>
2022-11-27 18:53:17 -0500

    Update Spaceship.java

---
+++ src/Spaceship.java
@@ -0,0 +1,16 @@
+/** Video 1: Overview of our plan - https://www.youtube.com/watch?v=LTHNYZ7RZNU&list=PLpkMWkvwtatg_BHn3tb3FsXklJz4EurHH&index=7
+ *  Video 2: Creating variables/constructors - https://www.youtube.com/watch?v=gleqYdOr_Fo
+ * 
+ *  Task: 
+ *  Create a Spaceship that:
+ *  -has health variable
+ *  -has a shape (stored in ArrayList)
+ *  -has speed variable
+ *  -has direction variable
+ * 
+ *  With constructors:
+ *  1. Creates Spaceship with default values for health (10), shape (two Ovals), speed (5)
+ * 
+ *  2. Creates Spaceship where health and speed are passed in as parameters
+ *
+ */
