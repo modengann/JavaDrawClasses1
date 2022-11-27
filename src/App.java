8db9927928fa07327f557534633f10dfda15ca38
modengann <99417989+modengann@users.noreply.github.com>
2022-11-27 17:29:35 -0500

    init

---
+++ src/App.java
@@ -0,0 +1,31 @@
+/** Video 1: Overview of our plan - https://www.youtube.com/watch?v=LTHNYZ7RZNU&list=PLpkMWkvwtatg_BHn3tb3FsXklJz4EurHH&index=7
+ *  Video 2: Creating variables - 
+ * 
+ *  Task: 
+ *  Create a Spaceship that:
+ *  -has health variable
+ *  -has a shape (stored in ArrayList)
+ *  -has speed variable
+ *  -has direction variable
+ * 
+ *  With constructors:
+ *  1. Creates Spaceship with default values for health (10), shape (two Ovals), speed (5)
+ * 
+ *  2. Creates Spaceship where health and speed are passed in as parameters
+ * 
+ *  
+ * 
+ */
+import javadraw.*;
+import java.util.*;
+
+
+public class App extends Window{
+    public static void main(String[] args) throws Exception {
+        Window.open();
+    }
+
+    public void start(){
+
+    }
+}
